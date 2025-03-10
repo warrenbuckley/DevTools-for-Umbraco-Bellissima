b5e136691da34fc6f2adcf11924188f355be274c
Warren Buckley <warren@hackmakedo.com>
2025-03-10 13:49:14 +0000

    Seems with Chromium the DOM was not finished loading & did not find umb-app anymore

--- src/devtools/devtools.registration.ts
+++ src/devtools/devtools.registration.ts
@@ -1,10 +1,29 @@
 import browser from "webextension-polyfill";
 
+// Wait for the DOM to finish loading
+// This is to ensure that the content script can find the <umb-app> element in the DOM
+// before we check if it is there or not
+
+const checkDOMReadyAndFindUmbApp = `
+    (function() {
+        if (document.readyState === 'complete' || document.readyState === 'interactive') {
+            return document.getElementsByTagName('umb-app').length;
+        } else {
+            document.addEventListener('DOMContentLoaded', function() {
+                return document.getElementsByTagName('umb-app').length;
+            });
+        }
+    })();
+`;
+
+
 // Check if we can find the <umb-app> element in the page
 // We check the length rather than getting the DOM element itself as it can NOT be serialized
 // as JSON over the DevTools protocol. So hence a simple check for the length of the array
-browser.devtools.inspectedWindow.eval("document.getElementsByTagName('umb-app').length").then((result) => {
+browser.devtools.inspectedWindow.eval(checkDOMReadyAndFindUmbApp).then((result) => {
     
+    console.log('result of get elment by tag name.length', result);
+
     // We only expect the result of the JS call above to be 0 or 1
     // The result is an array of values that contains the result of the JS call and the other is any errors
     const valueOfEval = result[0];
